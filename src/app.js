836a2fb2d67b93ab76a56f6c75bea9902c4e6474
Ashish Kumar gupta <97165456+ashishit07@users.noreply.github.com>
2025-07-29 19:21:37 +0530

    update

--- src/app.js
+++ src/app.js
@@ -3,9 +3,23 @@ const express = require("express")
 const app = express();
 
 
-app.use("/dashboard",(req,res) => {
-    res.send(req.body)
-});
+
+
+app.get("/user", (req,res) => {
+    res.send("i am from get user")
+})
+
+
+app.post("/user:firstName", (req,res) => {
+    console.log(req.query);
+    
+    res.send("i m from post call")
+})
+
+app.delete("/user", (req, res) => {
+    res.send("hello i m from delete server")
+})
+
 
 app.listen(3000,() =>{
     console.log("server is running");
