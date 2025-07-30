3cfcbf45a168888f769145f09dc5fbeb6c75785b
Ashish Kumar gupta <97165456+ashishit07@users.noreply.github.com>
2025-07-30 08:18:11 +0530

    playing with routes

--- src/app.js
+++ src/app.js
@@ -5,23 +5,33 @@ const app = express();
 
 
 
-app.get("/user", (req,res) => {
-    res.send("i am from get user")
-})
+// app.use("/user/2", (req, res) => {
+//     res.send("hello hello bab")
+// })
 
 
-app.post("/user:firstName", (req,res) => {
-    console.log(req.query);
+app.post("/user/:userID/:name/:password", (req,res) => {
+    console.log(req.params);
+     res.send({firstName: "Ashissh" , lastName: "Gupta"})
+});
+
+// app.get("/user", (req,res) => {
+//     res.send("i am from get user")
+// })
+
+
+// app.post("/user:firstName", (req,res) => {
+//     console.log(req.query);
     
-    res.send("i m from post call")
-})
+//     res.send("i m from post call")
+// })
 
-app.delete("/user", (req, res) => {
-    res.send("hello i m from delete server")
-})
+// app.delete("/user", (req, res) => {
+//     res.send("hello i m from delete server")
+// })
 
 
 app.listen(3000,() =>{
-    console.log("server is runninggg");
+    console.log("server is running");
     
 });
